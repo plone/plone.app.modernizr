2fa480fd6b84d56c0f7bf3ffaca8aedc4336207d
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-03-11 16:33:23 +0100

    remove unecessary complexity

--- plone/app/modernizr/resources.py
+++ plone/app/modernizr/resources.py
@@ -25,84 +25,10 @@ TESTS += "teststyles-testprop-testallprops-hasevent-prefixes-domprefixes-load"
 class IModernizr(interface.Interface):
     """Modernizr settings"""
 
-    resource = schema.ASCIILine(title=u"Resource ID",
-                    default="++resource++plone.app.modernizr/modernizr.js")
-
     tests = schema.Tuple(title=u"Tests",
                     description=u"Write only the tests you need.",
                     value_type=schema.ASCIILine(title=u"Test"),
                     default=tuple())
-
-
-class ModernizrCustomJS(BrowserView):
-    """Return the content of the configured resource name"""
-
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
-
-        #deps
-        self.portal_resgistry = None
-        self.settings = None
-
-    def update(self):
-        if self.portal_resgistry is None:
-            self.portal_resgistry = component.queryUtility(IRegistry)
-        if self.settings is None:
-            forInterface = self.portal_resgistry.forInterface
-            self.settings = forInterface(IModernizr, None)
-
-    def __call__(self):
-        self.update()
-        if self.settings is None:
-            return
-        response = self.request.response
-        response.setHeader('Content-Type', 'application/javascript')
-
-        resourceid = self.settings.resource
-        return self.get_resource_content(resourceid)
-
-    def get_resource_content(self, resourceid):
-        """Resources must be a list of resource ids.
-        This method return the content of each resources appended in one string
-        """
-        data = StringIO()
-        try:
-            resource = self.context.restrictedTraverse(resourceid)
-        except KeyError, e:
-            logger.error(e)
-        if not resource:
-            return ""
-
-        if type(aq_base(resource)) == File:
-            #is persistent resource
-            rname = resource.__name__
-            content = "%s" % resource
-        else:
-            #is browser:resource
-            rname = resource.context.__name__
-            rpath = resource.context.path
-            fic = open(rpath, 'r')
-            content = fic.read()
-            fic.close()
-
-        data.write(
-            u"\n/* plone.app.modernizr: %s */\n" % rname
-        )
-        data.write(u"\n")
-        try:
-            content = unicode(content)
-        except Exception, e:
-            content = unicode(
-                content.decode('utf-8'))
-        data.write(content)
-        data.write(u"\n")
-        return data.getvalue()
-
-
-class PersistentModernizr(object):
-    """peristent version of modernizr"""
-    interface.implements()
 
 
 class Download(BrowserView):
@@ -127,4 +53,4 @@ class Download(BrowserView):
             tests = '-'.join(self.settings.tests)
         url = self.BASE + tests
         self.request.response.redirect(url)
-        return "download"
+        return ""
