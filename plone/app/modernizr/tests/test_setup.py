1a6b12c287b4be6fd76171fc7825763881852d74
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-03-26 10:42:35 +0100

    remove upgrade step. it has to be done in plone.app.ugprades

--- plone/app/modernizr/tests/test_setup.py
+++ plone/app/modernizr/tests/test_setup.py
@@ -1,7 +1,7 @@
 from plone.app.modernizr.tests import base
 
 OLD_JS = 'modernizr.js'
-JS = '++resource++plone.app.modernizr/modernizr.js'
+JS = '++resource++plone.app.modernizr.custom.js'
 PROFILE = 'plone.app.modernizr:default'
 
 class TestIntegration(base.TestCase):
@@ -12,20 +12,6 @@ class TestIntegration(base.TestCase):
         self.failUnless(not modernizr.getEnabled())
         new_modernizr = jsregistry.getResource(JS)
         self.failUnless(new_modernizr.getEnabled())
-
-    def test_upgrade(self):
-        from Products.GenericSetup.upgrade import listUpgradeSteps
-        setup = self.portal.portal_setup
-        steps = listUpgradeSteps(setup, PROFILE, None)
-        step_id = steps[0]['id']
-        request = self.portal.REQUEST
-        request.form['upgrades'] = [step_id]
-        request.form['profile_id'] = PROFILE
-        setup.manage_doUpgrades()
-        
-        jsregistry = self.portal.portal_javascripts
-        modernizr = jsregistry.getResource(OLD_JS)
-        self.failUnless(modernizr is None)
 
 
 def test_suite():
--- plone/app/modernizr/upgrades.py
+++
@@ -1,7 +0,0 @@
-from Products.CMFCore.utils import getToolByName
-PROFILE = 'profile-plone.app.modernizr:default'
-
-
-def common(context):
-    setup = getToolByName(context, 'portal_setup')
-    setup.runAllImportStepsFromProfile(PROFILE)
